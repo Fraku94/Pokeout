526983c79614bb31652e306568272c6f20d74061
[bartlomiejgorzelak] <[bartlomiej.gorzelak@gmail.com]>
2018-02-20 11:43:19 +0100

    Rejestracja logowanie tla

--- app/src/main/java/com/example/pokeout/pokeout/MainActivity.java
+++ app/src/main/java/com/example/pokeout/pokeout/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.pokeout.pokeout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public void Logout(View view) {
+        auth.signOut();
+        Intent intent =new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+        return;
     }
 }
